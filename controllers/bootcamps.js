5a07aa341d1c9cc7b7e938b6d53b37611b8a7c8e
Nadia <nadiahosisy@gmail.com>
2023-12-05 21:31:31 +0200

    add

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,8 +3,14 @@ const Bootcamp = require("../models/Bootcamp");
 //@desc       Get all bootcamps
 //@route      GET /api/vi/bootcamps
 //@access     Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@desc       Get single bootcamp
@@ -20,12 +26,18 @@ exports.getBootcamp = (req, res, next) => {
 //@route      POST /api/vi/bootcamps
 //@access     Private
 exports.createBootcamp = async (req, res, next) => {
-  const bootcamp = await Bootcamp.create(req.body);
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
 
-  res.status(201).json({
-    success: true,
-    data: bootcamp,
-  });
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 //@desc       Update bootcamp
