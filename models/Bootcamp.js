c9220f1da6727dc0053835deb3ec8bbe220befdd
Nadia <nadiahosisy@gmail.com>
2023-12-05 12:31:36 +0200

    added Schema

---
+++ models/Bootcamp.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const BootcamSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please add a name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name can not be more than 50 characters"],
+  },
+  slug: string,
+  description: {
+    type: String,
+    required: [true, "please add a description"],
+    maxlength: [500, "Name can not be more than 500 characters"],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      "Please use a valid URL with HTTP or HTTPS",
+    ],
+  },
+});
