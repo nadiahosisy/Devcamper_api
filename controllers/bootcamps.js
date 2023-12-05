06c70495427b15db596844e616865e43eda0a146
Nadia <nadiahosisy@gmail.com>
2023-12-05 14:28:30 +0200

    add

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -20,6 +20,7 @@ exports.getBootcamp = (req, res, next) => {
 //@route      POST /api/vi/bootcamps
 //@access     Private
 exports.createBootcamp = (req, res, next) => {
+  console.log("*****", req.body);
   res.status(200).json({ success: true, msg: "Create new bootcamp" });
 };
 
