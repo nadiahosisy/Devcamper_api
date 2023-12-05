3de0ba560825796f6c549afc6054f44f5d299967
Nadia <nadiahosisy@gmail.com>
2023-12-05 11:42:29 +0200

    connect to DB

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
-//Route files
-const bootcamps = require("./routes/bootcamps");
-
-//LOAD env Vars
+// LOAD env Vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to the database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -14,12 +18,12 @@ const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-//Mount routers
 
+// Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-  PORT,
-  console.log(`server running  in ${process.env.NODE_ENV} mode on port ${PORT}`)
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
