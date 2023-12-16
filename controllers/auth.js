dcf3b67cabd1a19fa5aa9844389b25206d16ca90
Nadia <nadiahosisy@gmail.com>
2023-12-17 00:45:02 +0200

    add

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,10 +18,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  //Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 //@desc       Login user
@@ -50,11 +47,7 @@ exports.login = asyncHandler(async (req, res, next) => {
   if (!isMatch) {
     return next(new ErrorResponse("Invalid credentials", 401));
   }
-
-  //Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 //Get token from model,create cookie and send response
@@ -66,5 +59,15 @@ const sendTokenResponse = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
+    httpOnly: true,
   };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, token });
 };
