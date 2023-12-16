64a3f7d5d43acc479d8451bdcc49519de4ef96e7
Nadia <nadiahosisy@gmail.com>
2023-12-16 12:27:24 +0200

    added UserSchema

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  role: {
+    type: String,
+    enum: ["user", "publisher"],
+    defult: "user",
+  },
+  passowrd: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    defult: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
