06567ee3a38a1750d07641220e47244dae6c5de5
Nadia <nadiahosisy@gmail.com>
2023-12-17 13:27:51 +0200

    Grant access to specific roles

--- middleware/auth.js
+++ middleware/auth.js
@@ -25,13 +25,29 @@ exports.protect = asyncHandler(async (req, res, next) => {
   try {
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     console.log(decoded);
 
     req.user = await User.findById(decoded.id);
 
     next();
   } catch (error) {
-    return next(new ErrorResponse("Not authorize to access this route", 401));
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 });
+
+// Grant access to specific roles
+// Pass in a comma separated list of roles
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role '${req.user.role}' is not authorized to access this route`,
+          // Forbidden
+          403
+        )
+      );
+    }
+    next();
+  };
+};
--- routes/courses.js
+++ routes/courses.js
@@ -15,7 +15,7 @@ const router = express.Router({
   mergeParams: true,
 });
 
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
 router
   .route("/")
@@ -26,11 +26,11 @@ router
     }),
     getCourses
   )
-  .post(protect, addCourse);
+  .post(protect, authorize("publisher", "admin"), addCourse);
 router
   .route("/:id")
   .get(getCourse)
-  .put(protect, updateCourse)
-  .delete(deleteCourse);
+  .put(protect, authorize("publisher", "admin"), updateCourse)
+  .delete(protect, authorize("publisher", "admin"), deleteCourse);
 
 module.exports = router;
