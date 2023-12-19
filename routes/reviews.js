fb3dc7947f53f4b7d99eea276b2635fb92497daa
Nadia <nadiahosisy@gmail.com>
2023-12-19 12:48:11 +0200

    added review for boocamp

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { getReviews, getReview } = require("../controllers/reviews");
+const { getReviews, getReview, addReview } = require("../controllers/reviews");
 
 const Review = require("../models/Review");
 
@@ -11,13 +11,16 @@ const router = express.Router({
 const advancedResult = require("../middleware/advancedResult");
 const { protect, authorize } = require("../middleware/auth");
 
-router.route("/").get(
-  advancedResult(Review, {
-    path: "bootcamp",
-    select: "name description",
-  }),
-  getReviews
-);
+router
+  .route("/")
+  .get(
+    advancedResult(Review, {
+      path: "bootcamp",
+      select: "name description",
+    }),
+    getReviews
+  )
+  .post(protect, authorize("user", "admin"), addReview);
 router.route("/:id").get(getReview);
 
 module.exports = router;
